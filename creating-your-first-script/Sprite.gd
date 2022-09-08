ba014eb3d83ac80d7c77fc13372302243d05b8c9
Don <ngdon@pm.me>
2022-09-07 19:59:08 -0700

    Utilize user input

--- creating-your-first-script/Sprite.gd
+++ creating-your-first-script/Sprite.gd
@@ -3,10 +3,18 @@ extends Sprite
 var speed: float = 300
 var angular_speed: float = PI
 
+
 func _process(delta):
-  rotation += angular_speed * delta
-  
-  var velocity = Vector2.UP.rotated(rotation) * speed
+	var direction = 0
+	if Input.is_action_pressed("ui_left"):
+		direction = -1
+	if Input.is_action_pressed("ui_right"):
+		direction = 1
 
-  position += velocity * delta
+	rotation += angular_speed * delta * direction
 
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("ui_up"):
+		velocity = Vector2.UP.rotated(rotation) * speed
+
+	position += velocity * delta
